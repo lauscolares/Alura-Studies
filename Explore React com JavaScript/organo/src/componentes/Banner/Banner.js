eec296eda500a8a49bdddaa9044f1dd3e4fd7b60
Laura Colares <lauscolares@gmail.com>
2024-08-13 17:47:48 -0300

    Add initial project files and configurations

---
+++ Explore React com JavaScript/organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header classname="banner">
+            <img src="imagens/banner.png" alt="Banner principal"/>
+        </header>
+    )
+
+}
+
+export default Banner
