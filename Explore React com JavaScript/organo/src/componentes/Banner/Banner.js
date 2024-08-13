3d0c346c3d3509fac4c2dde1d12ed312f8bc52cc
Laura Colares <lauscolares@gmail.com>
2024-08-13 18:02:12 -0300

    Remove unnecessary test files and update banner class name

--- Explore React com JavaScript/organo/src/componentes/Banner/Banner.js
+++ Explore React com JavaScript/organo/src/componentes/Banner/Banner.js
@@ -2,7 +2,7 @@ import './Banner.css';
 
 function Banner() {
     return (
-        <header classname="banner">
+        <header className="banner">
             <img src="imagens/banner.png" alt="Banner principal"/>
         </header>
     )
