0da0246bc968605ee03b936fb71a14381d7dab67
Laura Colares <lauscolares@gmail.com>
2024-08-29 12:01:35 -0300

    Novo estudo: React Router

---
+++ Explore React com JavaScript/ola-mundo/src/App.js
@@ -0,0 +1,11 @@
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter />
+  )
+}
+
+export default App;
