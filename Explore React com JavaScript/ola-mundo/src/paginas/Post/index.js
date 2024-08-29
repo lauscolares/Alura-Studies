0f1b2024d1b6f2e149122fe1d4709f90ae544ef7
Laura Colares <lauscolares@gmail.com>
2024-08-29 18:24:58 -0300

    Pequenas correções e melhorias

--- Explore React com JavaScript/ola-mundo/src/paginas/Post/index.js
+++ Explore React com JavaScript/ola-mundo/src/paginas/Post/index.js
@@ -5,6 +5,8 @@ import ReactMarkdown from "react-markdown";
 import "./Post.css";
 import NaoEncontrada from "paginas/NaoEncontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import styles from "./Post.module.css";
+import PostCard from "componentes/PostCard";
 
 export default function Post() {
     const parametros = useParams();
@@ -15,6 +17,12 @@ export default function Post() {
     if (!post) {
         return <NaoEncontrada/>
     }
+
+    const postsRecomendados = posts
+        .filter((post) => post.id !== Number(parametros.id))
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 4);
+
 
     return (
         <Routes>
@@ -29,6 +37,16 @@ export default function Post() {
                                 {post.texto}
                             </ReactMarkdown>
                         </div>
+                    <h2 className={styles.tituloOutrosPosts}>
+                        Outros posts que você pode gostar:
+                    </h2>
+                    <ul className={styles.postsRecomendados}>
+                        {postsRecomendados.map((post) => (
+                                <li key={post.id}>
+                                    <PostCard post={post} />
+                                </li>
+                            ))}
+                    </ul>
                     </PostModelo>
                 }
                 />
